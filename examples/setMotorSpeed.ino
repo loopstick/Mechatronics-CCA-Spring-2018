dae74a69b78a3d51972132e2c321dce434ccbf02
Michael Shiloh <michael@michaelshiloh.com>
2017-01-25 11:16:53 -0800

    how to use analogWrite() to set the speed of a motor

---
+++ examples/setMotorSpeed.ino
@@ -0,0 +1,17 @@
+/* A very simple example showing how to use analogWrite() to set the speed of a motor.
+ * In this example nothing ever changes so it can all be done in setup()
+ * 
+ * 1/25/17 - Michael Shiloh - Initial creation
+ * 
+ * Pin usage: 
+ * 3 - a transistor controlling a motor
+ */
+ 
+void setup() {
+  pinMode (3, OUTPUT); // strictly speaking not necessary but good practice for clarity
+  analogWrite(3, 40)    // Set the PWM value to 40 (can range from minimum 0 to maximum 255) 
+}
+  
+// Must always have a loop() function, even if there is nothing in it
+void loop(){
+}
